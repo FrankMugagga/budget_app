e6abbac0a831bb2ac56b3036f0af6e399bbc9a76
FrankMugagga <frank.mugagga@gmail.com>
2024-06-13 22:17:55 +0300

    Create resource controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
+  get 'expenses/index'
+  get 'expenses/show'
+  get 'expenses/new'
+  get 'expenses/create'
+  get 'expenses/edit'
+  get 'expenses/update'
+  get 'expenses/destroy'
+  get 'splash/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
